cf485f2e56feba69044536b59497788d75740023
cee-mack <mackenzie.craiga@gmail.com>
2020-02-21 05:43:27 +0000

    added checks before deleting id, and logging

--- src/dynamo_utils.py
+++ src/dynamo_utils.py
@@ -1,6 +1,10 @@
 import boto3
 import time
 import os
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 region = os.environ['REGION']
 
@@ -10,23 +14,35 @@ table = dynamodb.Table('cdk-twitter-dynamo')
 
 
 def write_last_id(id):
-    delete_last_item()
-    table.put_item(
+    last_id = get_latest_id()
+
+    put_id = table.put_item(
         Item={
             'id': str(id),
             'timestamp': int(time.time())
         }
     )
+    if put_id['ResponseMetadata']['HTTPStatusCode'] == 200:
+        logger.info(f'Successfully put id {id} into dynamoDb')
+        delete_id = delete_last_item(last_id)
+        if delete_id['ResponseMetadata']['HTTPStatusCode'] == 200:
+            logger.info(f'Successfully deleted id {last_id} from dynamoDb')
+        else:
+            logger.error(f'Error deleting id {last_id} from dynamoDb')
+    else:
+        logger.error(f'Could not put latest id {id} into dynamoDb, previous ID remains')
 
 
 def get_latest_id():
     data = table.scan()
+
     return data['Items'][0]['id']
 
 
-def delete_last_item():
-    table.delete_item(TableName='cdk-twitter-dynamo',
-                      Key={
-                          'id': get_latest_id()
-                      }
-                      )
+def delete_last_item(id):
+    delete = table.delete_item(TableName='cdk-twitter-dynamo',
+                               Key={
+                                   'id': id
+                               }
+                               )
+    return delete
