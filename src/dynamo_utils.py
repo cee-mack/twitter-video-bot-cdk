bda2e0936cc3cec9512433cb8dd4a9ca99a923b4
cee-mack <mackenzie.craiga@gmail.com>
2020-04-14 21:06:26 +0100

    refactored dynamo utils to create document for each user

--- src/dynamo_utils.py
+++ src/dynamo_utils.py
@@ -1,48 +1,79 @@
 import boto3
 import time
-import os
 import logging
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-region = os.environ['REGION']
-
 session = boto3.session.Session()
-dynamodb = session.resource('dynamodb', region)
-table = dynamodb.Table('cdk-twitter-dynamo')
+dynamodb = session.resource('dynamodb', 'eu-west-1')
+table = dynamodb.Table('play')
 
 
-def write_last_id(id):
-    last_id = get_latest_id()
+def write_tweet(username, tweet_id):
+    user_document = get_document(username)
 
+    if 'Item' in user_document:
+        update_user_document(username, tweet_id)
+    else:
+        put_new_user_document(username, tweet_id)
+
+
+def return_latest_tweet_id():
+    latest = get_document('latest_tweet_id')
+    return latest['Item']['id']
+
+
+def write_latest_tweet_id(latest_tweet_id):
+    update = table.update_item(
+        Key={
+            'username': 'latest_tweet_id'
+        },
+        UpdateExpression="SET id = :i",
+        ExpressionAttributeValues={
+            ':i': latest_tweet_id,
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+
+    return update
+
+
+def get_document(username):
+    document = table.get_item(
+        Key={
+            'username': username
+        })
+
+    return document
+
+
+def update_user_document(username, tweet_id):
+    update = table.update_item(
+        Key={
+            'username': username
+        },
+        UpdateExpression="SET tweet = list_append(tweet, :i)",
+        ExpressionAttributeValues={
+            ':i': [{'id': tweet_id,
+                    'created': int(time.time())
+                    }],
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+
+    return update
+
+
+def put_new_user_document(username, tweet_id):
     put_id = table.put_item(
         Item={
-            'id': str(id),
-            'timestamp': int(time.time())
+            'username': username,
+            'tweet': [
+                {'id': tweet_id,
+                 'created': int(time.time())
+                 }
+            ]
         }
     )
-    if put_id['ResponseMetadata']['HTTPStatusCode'] == 200:
-        logger.info(f'Successfully put id {id} into dynamoDb')
-        delete_id = delete_last_item(last_id)
-        if delete_id['ResponseMetadata']['HTTPStatusCode'] == 200:
-            logger.info(f'Successfully deleted id {last_id} from dynamoDb')
-        else:
-            logger.error(f'Error deleting id {last_id} from dynamoDb')
-    else:
-        logger.error(f'Could not put latest id {id} into dynamoDb, previous ID remains')
-
-
-def get_latest_id():
-    data = table.scan()
-
-    return data['Items'][0]['id']
-
-
-def delete_last_item(id):
-    delete = table.delete_item(TableName='cdk-twitter-dynamo',
-                               Key={
-                                   'id': id
-                               }
-                               )
-    return delete
+    return put_id
