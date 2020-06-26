ff42cfdae636c831b015b5ac69a6cefc8d0c3f92
cee-mack <mackenzie.craiga@gmail.com>
2020-06-26 22:42:34 +0100

    tidied up imports

--- lib/cdk-twitter-stack.ts
+++ lib/cdk-twitter-stack.ts
@@ -1,14 +1,15 @@
-import events = require('@aws-cdk/aws-events');
-import targets = require('@aws-cdk/aws-events-targets');
-import lambda = require('@aws-cdk/aws-lambda');
+import { Rule, Schedule } from '@aws-cdk/aws-events';
+import { LambdaFunction as LambdaFunctionTarget } from '@aws-cdk/aws-events-targets';
+import { Code, Runtime, Function as LambdaFunction } from '@aws-cdk/aws-lambda';
 import { AwsIntegration, Cors, RestApi } from '@aws-cdk/aws-apigateway';
-import ssm = require('@aws-cdk/aws-ssm');
-import iam = require('@aws-cdk/aws-iam');
-import dynamodb = require('@aws-cdk/aws-dynamodb');
-import path = require('path');
+import { StringParameter } from '@aws-cdk/aws-ssm';
+import { Role, ServicePrincipal } from  '@aws-cdk/aws-iam';
+import { Table, AttributeType, BillingMode } from '@aws-cdk/aws-dynamodb';
+import { RemovalPolicy } from '@aws-cdk/core';
+import { PolicyStatement } from "@aws-cdk/aws-iam";
 import * as cdk from '@aws-cdk/core';
-import {RemovalPolicy} from '@aws-cdk/core';
-import {PolicyStatement} from "@aws-cdk/aws-iam";
+
+import path = require('path');
 
 export class CdkTwitterStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -22,45 +23,45 @@ export class CdkTwitterStack extends cdk.Stack {
         const apiName = 'TwitterAppApi';
         const pythonPath = '/var/task/dependencies:/var/runtime';
 
-        const accessToken = ssm.StringParameter.fromStringParameterAttributes(this, 'accessToken', {
+        const accessToken = StringParameter.fromStringParameterAttributes(this, 'accessToken', {
             parameterName: '/twitterlambda/accesstoken',
         }).stringValue;
 
-        const accessTokenSecret = ssm.StringParameter.fromStringParameterAttributes(this, 'accessTokenSecret', {
+        const accessTokenSecret = StringParameter.fromStringParameterAttributes(this, 'accessTokenSecret', {
             parameterName: '/twitterlambda/accesstokensecret',
         }).stringValue;
 
-        const consumerKey = ssm.StringParameter.fromStringParameterAttributes(this, 'consumerKey', {
+        const consumerKey = StringParameter.fromStringParameterAttributes(this, 'consumerKey', {
             parameterName: '/twitterlambda/consumerkey',
         }).stringValue;
 
-        const consumerSecret = ssm.StringParameter.fromStringParameterAttributes(this, 'consumerSecret', {
+        const consumerSecret = StringParameter.fromStringParameterAttributes(this, 'consumerSecret', {
             parameterName: '/twitterlambda/consumersecretkey',
         }).stringValue;
 
-        const searchString = ssm.StringParameter.fromStringParameterAttributes(this, 'searchString', {
+        const searchString = StringParameter.fromStringParameterAttributes(this, 'searchString', {
             parameterName: '/twitterlambda/searchstring',
         }).stringValue;
 
-        const twitterAccountName = ssm.StringParameter.fromStringParameterAttributes(this, 'twitterAccountName', {
+        const twitterAccountName = StringParameter.fromStringParameterAttributes(this, 'twitterAccountName', {
             parameterName: '/twitterlambda/twitteraccountname',
         }).stringValue;
 
-        const expiration = ssm.StringParameter.fromStringParameterAttributes(this, 'expiration', {
+        const expiration = StringParameter.fromStringParameterAttributes(this, 'expiration', {
             parameterName: '/twitterlambda/expiration',
         }).stringValue;
 
 
-        const twitterLambdaRole = new iam.Role(this, 'twitterLambdaRole', {
-            assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')
+        const twitterLambdaRole = new Role(this, 'twitterLambdaRole', {
+            assumedBy: new ServicePrincipal('lambda.amazonaws.com')
         });
 
-        const apiRole = new iam.Role(this, 'apiRole', {
-            assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com')
+        const apiRole = new Role(this, 'apiRole', {
+            assumedBy: new ServicePrincipal('apigateway.amazonaws.com')
         });
 
-        const dynamoLambdaRole = new iam.Role(this, 'dynamoLambdaRole', {
-            assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')
+        const dynamoLambdaRole = new Role(this, 'dynamoLambdaRole', {
+            assumedBy: new ServicePrincipal('lambda.amazonaws.com')
         });
 
         twitterLambdaRole.addToPolicy(new PolicyStatement({
@@ -100,12 +101,12 @@ export class CdkTwitterStack extends cdk.Stack {
                 'dynamodb:GetItem']
         }));
 
-        const twitterLambdaFunction = new lambda.Function(this, twitterLambdaName, {
+        const twitterLambdaFunction = new LambdaFunction(this, twitterLambdaName, {
             functionName: twitterLambdaName,
-            code: lambda.Code.fromAsset(path.join(__dirname, '../app/src')),
+            code: Code.fromAsset(path.join(__dirname, '../app/src')),
             role: twitterLambdaRole,
             handler: 'twitter_lambda.main.handler',
-            runtime: lambda.Runtime.PYTHON_3_8,
+            runtime: Runtime.PYTHON_3_8,
             timeout: cdk.Duration.seconds(10),
             environment: {
                 'CONSUMERKEY': consumerKey,
@@ -120,12 +121,12 @@ export class CdkTwitterStack extends cdk.Stack {
             }
         });
 
-        const dynamoLambdaFunction = new lambda.Function(this, dynamoLambdaName, {
+        const dynamoLambdaFunction = new LambdaFunction(this, dynamoLambdaName, {
             functionName: dynamoLambdaName,
-            code: lambda.Code.fromAsset(path.join(__dirname, '../app/src')),
+            code: Code.fromAsset(path.join(__dirname, '../app/src')),
             role: dynamoLambdaRole,
             handler: 'dynamo_lambda.main.handler',
-            runtime: lambda.Runtime.PYTHON_3_8,
+            runtime: Runtime.PYTHON_3_8,
             timeout: cdk.Duration.seconds(10),
             environment: {
                 'REGION': region,
@@ -134,17 +135,17 @@ export class CdkTwitterStack extends cdk.Stack {
             }
         });
 
-        const twitterLambdaRule = new events.Rule(this, 'twitterLambdaRule', {
-            schedule: events.Schedule.expression('cron(0/1 * * * ? *)')
+        const twitterLambdaRule = new Rule(this, 'twitterLambdaRule', {
+            schedule: Schedule.expression('cron(0/1 * * * ? *)')
         });
 
-        twitterLambdaRule.addTarget(new targets.LambdaFunction(twitterLambdaFunction));
+        twitterLambdaRule.addTarget(new LambdaFunctionTarget(twitterLambdaFunction));
 
-        new dynamodb.Table(this, dynamoTableName, {
+        new Table(this, dynamoTableName, {
             tableName: dynamoTableName,
             removalPolicy: RemovalPolicy.DESTROY,
-            partitionKey: {name: 'username', type: dynamodb.AttributeType.STRING},
-            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+            partitionKey: {name: 'username', type: AttributeType.STRING},
+            billingMode: BillingMode.PAY_PER_REQUEST,
             timeToLiveAttribute: 'expiry'
         });
 
