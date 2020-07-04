8cdda30b16c0e46cf850fcd7e97828d717ee9cd3
cee-mack <mackenzie.craiga@gmail.com>
2020-07-04 20:49:46 +0100

    added react app

---
+++ app/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import TweetCard from './TweetCard'
+
+function App() {
+  return (
+    <div className="App">
+      <TweetCard />
+    </div>
+  );
+}
+
+export default App;
